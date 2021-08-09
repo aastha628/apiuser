c95789d2cbf167f8364888a5c1aa3e99f14b71b9
Aastha Goyal <58726119+aastha628@users.noreply.github.com>
2021-08-09 15:16:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from dns.rdatatype import NULL
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.secret_key="secretkey"
+
+def mongopostdata(user):
+    app.config["MONGO_URI"]="mongodb+srv://aastha18:thunderbird@cluster0.a0epe.mongodb.net/formfill?retryWrites=true&w=majority"
+    mongo=PyMongo(app)
+    id = mongo.db.firstform.save(user)
+    if id!=NULL:
+        return 1
+    else:
+        return 0
+    
+
+@app.route('/postdata', methods=["POST"])
+def req_data():
+    request_body =request.get_json()
+
+    user={
+        "name":request_body['name'],
+        "email":request_body['email'],
+        "phone":request_body['phone'],
+        "gender":request_body['gender'],
+        "location":request_body['location']
+    }
+
+    value=mongopostdata(user)
+    if value==1:
+        return "Data saved Successfully"
+    else:
+        return "Error in saving the data"
+
+
+if __name__ =="__main__":
+    app.run()
